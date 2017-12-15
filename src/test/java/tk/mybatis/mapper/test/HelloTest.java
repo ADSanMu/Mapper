df3f076b0bf11f3420e60819c930341060c809b5
a637182 <Jiacheng_Deng@ssga.com>
2017-12-15 09:46:39 +0800

    changing #2

--- src/test/java/tk/mybatis/mapper/test/HelloTest.java
+++ src/test/java/tk/mybatis/mapper/test/HelloTest.java
@@ -5,6 +5,15 @@ import tk.mybatis.mapper.mapper.MybatisHelper;
 import tk.mybatis.mapper.mapperhelper.SqlHelper;
 import tk.mybatis.mapper.model.Country;
 
+/**
+ * TODO 多表之间的关系只做查询而不做任何update的操作
+ *
+ *  级联关系维护在vo上，实体类只维护本表的字段
+ *
+ *  做法：修改ms来：本质添加原来在xml配置文件所生成的部分动态添加到mapper方法中的
+ *  实例中
+ *
+ */
 public class HelloTest {
 
 
